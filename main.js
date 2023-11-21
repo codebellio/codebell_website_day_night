b032a71e62497917b873a1a8117b7aedccf59405
Vishnu Sharma <vs510514@gmail.com>
2023-11-21 22:26:01 +0530

    animation added in index page

--- main.js
+++ main.js
@@ -119,3 +119,9 @@ let reviewCustomer = new Swiper(".reviews-feedback", {
     prevEl: ".swiper-button-prev",
   },
 });
+// ---on scrol animation ----
+AOS.init( {
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 500,// values from 0 to 3000, with step 50ms
+});
