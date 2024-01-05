a7a47bb218b6567c4c4537f7737fc69be0cf62ad
Vishnu Sharma <vs510514@gmail.com>
2024-01-05 16:38:40 +0530

    offset update

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // ---on scroll animation ----
 AOS.init( {
-  offset: 100, // offset (in px) from the original trigger point
+  offset: 80, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 500,// values from 0 to 3000, with step 50ms
 });
