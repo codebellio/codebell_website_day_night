a4b952bf8c387e6876194e508f1c5e769264dee0
Vishnu Sharma <163235360+Bishnucodebell@users.noreply.github.com>
2024-05-23 20:30:45 +0530

    topbar, animation update

--- dist/newscript1.dev.js
+++ dist/newscript1.dev.js
@@ -5,8 +5,9 @@ AOS.init({
   // offset (in px) from the original trigger point
   delay: 0,
   // values from 0 to 3000, with step 50ms
-  duration: 1250 // values from 0 to 3000, with step 50ms
-
+  duration: 500,
+  // values from 0 to 3000, with step 50ms
+  easing: 'ease-out'
 }); // ---------------------FAQ----------------------------
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -84,15 +85,16 @@ englishBtn.addEventListener('click', toggleOutline);
 hindiBtn.addEventListener('click', toggleOutline); // -----sidebar------
 
 document.getElementById("menu-icon").addEventListener("click", function () {
-  document.getElementById("sidebar").style.width = "250px";
+  var sidebar = document.getElementById("sidebar");
+  sidebar.style.maxHeight = sidebar.scrollHeight + "px";
   document.getElementById("overlay").classList.add("active");
 });
 document.getElementById("overlay").addEventListener("click", function () {
-  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("sidebar").style.maxHeight = "0";
   this.classList.remove("active");
 });
 document.getElementById("close-btn").addEventListener("click", function () {
-  document.getElementById("sidebar").style.width = "0";
+  document.getElementById("sidebar").style.maxHeight = "0";
   document.getElementById("overlay").classList.remove("active");
 }); // -----------on scroll navbar --------------------------------
 
--- newscript1.js
+++ newscript1.js
@@ -1,7 +1,8 @@
 AOS.init({
   offset: 50, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
-  duration: 1250,// values from 0 to 3000, with step 50ms
+  duration: 500,// values from 0 to 3000, with step 50ms
+  easing: 'ease-out' 
 });
 // ---------------------FAQ----------------------------
 document.addEventListener("DOMContentLoaded", function () {
@@ -99,20 +100,20 @@ let reviewCustomers = new Swiper(".swiper-container-store", {
 
   // -----sidebar------
   document.getElementById("menu-icon").addEventListener("click", function() {
-    document.getElementById("sidebar").style.width = "250px";
+    let sidebar = document.getElementById("sidebar");
+    sidebar.style.maxHeight = sidebar.scrollHeight + "px";
     document.getElementById("overlay").classList.add("active");
 });
 
 document.getElementById("overlay").addEventListener("click", function() {
-    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("sidebar").style.maxHeight = "0";
     this.classList.remove("active");
 });
 
 document.getElementById("close-btn").addEventListener("click", function() {
-    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("sidebar").style.maxHeight = "0";
     document.getElementById("overlay").classList.remove("active");
 });
-
 
 // -----------on scroll navbar --------------------------------
 document.addEventListener('DOMContentLoaded', () => {
